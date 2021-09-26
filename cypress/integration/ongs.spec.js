d888b2189604a6a11ceb881f6cfa8f9b01e95706
Petherson Erasmo <petherson_erasmo@hotmail.com>
2021-09-26 19:29:04 -0300

    Implementa segundo dia de automação com Cypress da Agilizei
    Foi realizado a automação da função de logout, cadastrar um novo “caso” e exclui o “caso”, no final da aula foi informado de boas práticas e add o data-cy no HTML, porém, decidi não implementar isso, pois, nem sempre teremos essa possibilidade.

--- cypress/integration/ongs.spec.js
+++ cypress/integration/ongs.spec.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Ongs', () => {
-    it.skip('devem poder realizar um cadastro', () => {
+    it('devem poder realizar um cadastro', () => {
         cy.visit('http://localhost:3000/register');
         // cy.get - busca um elemento
         // .type - insere um texto
@@ -30,13 +30,52 @@ describe('Ongs', () => {
     });
 
     it('deve poder realizar um login no sistema', () => {
-
         // const createOngId = Cypress.env('createdOngId');
-
         // cy.log(createOngId);
 
         cy.visit('http://localhost:3000/');
-        cy.get('input').type(Cypress.env('createdOngId'));
+        cy.get('[data-cy=id]').type(Cypress.env('createdOngId'));
+        cy.get('[data-cy=button-login]').click();
+    });
+
+    it('deve poder fazer logout', () => {
+        // a ação de login está no commands
+        cy.login();
+        cy.get('button').click();
+    });
+
+    it('deve cadastrar novos casos', () => {
+        // a ação de login está no commands
+        cy.login();
+
         cy.get('.button').click();
+
+        cy.get('[placeholder="Título do caso"]').type('Pet abandonado');
+        cy.get('textarea').type('Foi encontrado um Pet abandonado, perto do poste logo ali depois daquela parada.');
+        cy.get('[placeholder="Valor em reais"]').type('6600');
+
+        cy.route('POST', '**/incidents').as('newIncident');
+        cy.get('.button').click();
+
+        cy.wait('@newIncident').then((xhr) => {
+            expect(xhr.status).to.eq(200);
+            expect(xhr.response.body).has.property('id');
+            expect(xhr.response.body.id).is.not.null;
+        })
+    });
+
+    it('deve poder excluir um casos', () => {
+        cy.createNewIncident();
+        cy.login();
+
+        cy.route('DELETE', '**/incidents/*').as('deleteIncident');
+        cy.get('li > button > svg').click();
+
+        cy.wait('@deleteIncident').then((xhr) => {
+            expect(xhr.status).to.eq(204);
+            expect(xhr.response.body).to.be.empty;
+        })
     });
 });
+
+// ec61969f
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -42,3 +42,30 @@ Cypress.Commands.add("createOng", () => {
         Cypress.env('createdOngId', response.body.id);
     });
 })
+
+Cypress.Commands.add("login", () => {
+    cy.visit('http://localhost:3000/profile', {
+        onBeforeLoad: (navegador) => {
+            navegador.localStorage.setItem('ongId', Cypress.env('createdOngId'));
+            navegador.localStorage.setItem('ongName', 'Pets mais Queridos');
+        }
+    });
+})
+
+Cypress.Commands.add("createNewIncident", () => {
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/incidents',
+        headers: {'Authorization': `${Cypress.env('createdOngId')}`},
+        body: {
+            title: "Caso de Pete Perdido",
+            description: "Foi notificado que existe um Pet perdido no centro",
+            value: "222"
+        }
+    }).then(response => {
+        expect(response.body.id).is.not.null;
+        cy.log(response.body.id);
+
+        Cypress.env('createdIncidentId', response.body.id);
+    })
+})
